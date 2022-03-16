dcae2b234a66c89eeea0d025a63f4d90c765e887
Jonathan <steelsouljs@gmail.com>
2022-03-16 08:26:30 -0700

    Initial commit

---
+++ src/SolveTowers.java
@@ -0,0 +1,11 @@
+public class SolveTowers
+{
+    public static void main(String[]args)
+    {
+        //Classwork 8.0 - Towers of Hanoi - Jonathan Serrano
+        System.out.println("Classwork 8.0 - Towers of Hanoi - Jonathan Serrano");
+        TowersOfHanoi towers = new TowersOfHanoi (4);
+        towers.solve();
+    }
+
+}
